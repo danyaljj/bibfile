c96c4b3c480db92c1b1e3c832e877ac51b4a0c31
Daniel Khashabi <d.khashabi@gmail.com>
2022-06-02 12:14:57 -0700

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,5 +1,5 @@
 from pybtex.database import parse_file
-bib_data = parse_file('../ref.bib')
+bib_data = parse_file('ref.bib')
 
 for author in bib_data.entries['Knuth:TB8-1-14'].persons['author']:
   print(unicode(author))
