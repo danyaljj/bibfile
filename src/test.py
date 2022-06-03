957b18eb608b102690c3b0f079380df5f066fb2a
Daniel Khashabi <danielk@allenai.org>
2022-06-02 21:33:32 -0700

    update.

--- src/test.py
+++ src/test.py
@@ -1,5 +1,14 @@
 from pybtex.database import parse_file
+
+# make sure the file is valid and the keys are not duplicate
 bib_data = parse_file('ref.bib')
 
-for author in bib_data.entries['Knuth:TB8-1-14'].persons['author']:
-  print(unicode(author))
+for entries in bib_data.entries.keys():
+
+  # make sure they have "authors"
+  for author in bib_data.entries[entries].persons['author']:
+    print(f" * {entries} -> {author}")
+
+  # make sure they have "year"
+  year = bib_data.entries[entries].fields['year']
+  print(f" * {entries} -> {year}")
